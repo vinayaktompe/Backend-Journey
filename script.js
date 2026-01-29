2557dcb57d9ac48ecce0631b49b3ca9f9a6823af
vinayaktompe <vinayaktompe9579@gmail.com>
2026-01-29 15:53:51 +0530

    day-87

---
+++ script.js
@@ -0,0 +1,22 @@
+const express =require('express')
+
+const app =express()
+ 
+app.use(express.json())
+let notes =[]
+
+app.get('/notes',(req,res)=>{
+        res.send(notes)
+})
+
+
+app.post('/notes',(req,res)=>{
+
+     console.log(req.body)
+       notes.push(req.body)
+    res.send('notes crated')
+ })
+
+ app.listen(3000,()=>{
+           console.log('conected')   
+ })
